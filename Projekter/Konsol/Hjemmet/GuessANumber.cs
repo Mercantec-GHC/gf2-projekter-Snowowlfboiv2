8bc01cad2f72916cc28437afed223be83b922826
Snowowlfboiv2 <snowwolfboi1337@proton.me>
2025-09-17 12:29:40 +0200

    guessthegame
    finished

--- Projekter/Konsol/Hjemmet/GuessANumber.cs
+++ Projekter/Konsol/Hjemmet/GuessANumber.cs
@@ -5,9 +5,46 @@ namespace Hjemmet
         public void Start()
         {
             Console.WriteLine("Gæt et tal er ikke implementeret endnu.");
+
+            Random random = new Random();
+
+            int number = random.Next(1, 501);
+            int userGuess = 0;
+            int attempts = 0;
+
+            while (userGuess != number)
+            {
+                Console.WriteLine("Indtast dit gæt: ");
+                string input = Console.ReadLine();
+                bool isNumber = int.TryParse(input, out userGuess);
+
+                if (!isNumber)
+                {
+                    Console.WriteLine("Det var ikke et gyldigt tal. prøv igen");
+                    continue;
+                }
+
+                attempts++;
+
+                if (userGuess < number)
+                {
+                    Console.WriteLine("For lavt! Prøv et højere tal.");
+                }
+                else if (userGuess > number) 
+                {
+                    Console.WriteLine("For Højt! Præv et lavere tal.");
+                }
+                else
+                {
+                    Console.WriteLine($"Tillykke! Du gættede rigtigt på {attempts} forsøg.");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Tryk på en tast for at afslutte.");
             Console.ReadKey();
 
-            Console.WriteLine();
+
+
         }
     }
 }
